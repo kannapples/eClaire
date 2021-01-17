752903c3a700f562b2bddf9306e4eb8ff4313580
Claire Kannapell <ckannapell7@gmail.com>
2021-01-17 11:05:37 -0600

    render background image

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,13 @@ import React from "react"
 
 import SEO from "../components/seo"
 import NavigationBar from "../components/navigationBar"
+import Img from "gatsby-image"
 
-function IndexPage() {
+function IndexPage({ data }) {
   return (
     <div id="home-page-container" className="vw-100 vh-100 primary-color d-flex align-items-center justify-content-center body page">
       <SEO title="Home" />
+      <Img fixed={data.file.childImageSharp.fixed}/>
       <div id="left-pane" className="flex-container">
         <NavigationBar />
       </div>
@@ -15,3 +17,16 @@ function IndexPage() {
 }
 
 export default IndexPage
+
+
+export const query = graphql`
+  query {
+    file(relativePath: { eq: "BW_smiling_with_hat.png" }) {
+      childImageSharp {
+        fixed(width: 500, height: 500) {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    }
+  }
+`
