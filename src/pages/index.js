2a42a18007ed558602ec223aae8c5479c7561824
Claire Kannapell <ckannapell7@gmail.com>
2021-01-17 12:27:52 -0600

    add gatsby-background-image library, format bg image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,32 +1,49 @@
 import React from "react"
+import { graphql, useStaticQuery } from "gatsby"
+import styled from "styled-components"
+import BackgroundImage from "gatsby-background-image"
 
 import SEO from "../components/seo"
 import NavigationBar from "../components/navigationBar"
-import Img from "gatsby-image"
 
-function IndexPage({ data }) {
+
+
+const BackgroundSection = ({ className }) => {
+  const data = useStaticQuery(graphql`
+    query {
+      desktop: file(relativePath: { eq: "BW_smiling_with_hat.jpg" }) {
+        childImageSharp {
+          fluid(quality: 100, maxWidth: 1920) {
+            ...GatsbyImageSharpFluid_withWebp
+          }
+        }
+      }
+    }`
+  )
+
+  const imageData = data.desktop.childImageSharp.fluid
+
   return (
-    <div id="home-page-container" className="vw-100 vh-100 primary-color d-flex align-items-center justify-content-center body page">
-      <SEO title="Home" />
-      <Img fixed={data.file.childImageSharp.fixed}/>
-      <div id="left-pane" className="flex-container">
-        <NavigationBar />
-      </div>
-    </div>
+      <BackgroundImage
+        Tag="section"
+        className={className}
+        fluid={imageData}
+      >
+        <div id="home-page-container"> 
+          <SEO title="Home" />
+          <div id="left-pane" className="flex-container">
+            <NavigationBar />
+          </div>
+        </div>
+      </BackgroundImage>
   )
 }
 
-export default IndexPage
-
-
-export const query = graphql`
-  query {
-    file(relativePath: { eq: "BW_smiling_with_hat.png" }) {
-      childImageSharp {
-        fixed(width: 500, height: 500) {
-          ...GatsbyImageSharpFixed
-        }
-      }
-    }
-  }
+const IndexPage = styled(BackgroundSection)`
+  width: 100%;
+  background-position: middle center;
+  background-repeat: repeat-y;
+  background-size: cover;
 `
+
+export default IndexPage
