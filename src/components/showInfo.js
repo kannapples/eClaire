973798688e53e0e081550008fe9e39767d962617
Claire Kannapell <ckannapell7@gmail.com>
2021-04-18 14:39:18 -0500

    build upcoming show info blocks

--- src/components/showInfo.js
+++ src/components/showInfo.js
@@ -1,14 +1,30 @@
 import React from "react";
+import Image from "../components/Image";
 import "../stylesheets/showInfo.scss";
 
-export default function ShowInfo({date, time, location, price, description}) {
+export default function ShowInfo({date, time, location, address, price, description, imgSrc}) {
     return (
         <div className="show-info-container">
-            <span>{date}</span>
-            <span>{time}</span>
-            <span>{location}</span>
-            <span>{price}</span>
-            <span>{description}</span>
+            <div className="show-info-image">
+                <Image 
+                    src={imgSrc}
+                    alt="Old Oaks Logo"
+                />
+            </div>
+            <div className="show-info-details">
+                <span className="show-info-description">{description}</span>
+                <div className="show-info-block">
+                    <div className="show-info-date-time">
+                        <span className="show-info-tag">{date}</span>
+                        <span className="show-info-tag">{time}</span>
+                        <span className="show-info-tag">{price}</span>
+                    </div>
+                    <div className="show-info-address">
+                        <span className="show-info-tag">{location}</span>
+                        <span className="show-info-tag">{address}</span>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
--- src/pages/upcoming-shows.js
+++ src/pages/upcoming-shows.js
@@ -12,20 +12,49 @@ export default function UpcomingShows() {
             
             <div className="page-content">
                 <ShowInfo 
-                    title="Old Oaks Album Release Show"
-                    date="1/1/2020"
-                    time="7pm"
-                    location="Come Back Inn"
+                    title="'The Day Before Yesterday' Album Release Show (Madison)"
+                    date="Friday, May 28, 2021"
+                    time="6pm - 9pm"
+                    location="Come Back In"
+                    address={<>
+                        508 E Wilson St. <br/>
+                        Madison, WI 53703
+                    </>}
                     price={null}
-                    description="Come celebrate the release of Old Oaks first full-length album!"
+                    description={<>
+                        Come celebrate the release of Old Oaks’ first full-length album at Come Back In in Madison! <br/>
+                        This will be a night you wont want to miss, complete with some fantastic special guests.
+                    </>}
+                    imgSrc="OldOaksLogo_ColorFrame.png"
                 />
                 <ShowInfo 
-                    title="Old Oaks Album Release Show 2"
-                    date="1/1/2020"
-                    time="7pm"
+                    title="'The Day Before Yesterday' Album Release Show (Mt. Horeb)"
+                    date="Friday, June 04, 2021"
+                    time="5pm - 8pm"
                     location="Brix Cider"
+                    address={<>
+                        119 S 2nd St. <br/>
+                        Mt Horeb, WI 53572
+                    </>}
                     price={null}
-                    description="Come celebrate the release of Old Oaks first full-length album!"
+                    description={<>
+                        The release show so nice we did it twice! <br/>
+                        Come celebrate the release of Old Oaks’ first full-length album at Brix Cider in Mount Horeb!
+                    </>}
+                    imgSrc="OldOaksLogo_ColorFrame.png"
+                />
+                <ShowInfo 
+                    title="Old Oaks on the Sunn Cafe Patio"
+                    date="Saturday, August 28, 2021"
+                    time="12:30pm - 2:30pm"
+                    location="Sunn Cafe"
+                    address={<>
+                        201 E Main St. <br/>
+                        Mt Horeb, WI 53572
+                    </>}
+                    price={null}
+                    description="Join us for a leisurely Saturday lunch at our favorite Mount Horeb cafe!"
+                    imgSrc="OldOaksLogo_ColorFrame.png"
                 />
             </div>
         </div>
