98873773d1c134880042a805890888baadb1f85e
Claire Kannapell <ckannapell7@gmail.com>
2021-05-31 09:31:26 -0500

    build upcoming shows page

---
+++ src/components/monthMarker.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "../stylesheets/showInfo.scss";
+
+export default function MonthMarker({ month }) {
+    return (
+        <div class="month-marker">
+    
+        {month}
+        </div>
+    )    
+}
--- src/components/showInfo.js
+++ src/components/showInfo.js
@@ -2,29 +2,46 @@ import React from "react";
 import Image from "../components/Image";
 import "../stylesheets/showInfo.scss";
 
-export default function ShowInfo({date, time, location, address, price, description, imgSrc}) {
+export function ShowInfo({date, time, location, address, price, description, imgSrc, altText}) {
     return (
         <div className="show-info-container">
             <div className="show-info-image">
-                <Image 
-                    src={imgSrc}
-                    alt="Old Oaks Logo"
-                />
+                {imgSrc !== '' &&
+                    <Image 
+                        src={imgSrc}
+                        alt={altText}
+                    />
+                }
+                {imgSrc === '' &&
+                    <div className="band-logo-alt-text">{altText}</div>
+                }
             </div>
+            
             <div className="show-info-details">
-                <span className="show-info-description">{description}</span>
+                <div className="show-info-date-time">
+                    <span className="show-info-tag">{date}</span>
+                    <span className="show-info-tag">{time}</span>
+                </div>
                 <div className="show-info-block">
-                    <div className="show-info-date-time">
-                        <span className="show-info-tag">{date}</span>
-                        <span className="show-info-tag">{time}</span>
-                        <span className="show-info-tag">{price}</span>
-                    </div>
                     <div className="show-info-address">
                         <span className="show-info-tag">{location}</span>
                         <span className="show-info-tag">{address}</span>
+                        <span className="show-info-tag">{price}</span>
                     </div>
+                    <div className="show-info-description">{description}</div>
                 </div>
+                
             </div>
         </div>
     );
+}
+
+export function NoShowInfo({description}) {
+    return (
+        <div className="show-info-container">
+            <div className="show-info-details">
+                <span className="show-info-description">{description}</span>
+            </div>
+        </div>
+    )
 }
